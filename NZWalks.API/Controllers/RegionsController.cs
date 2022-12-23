298f04c72827b435119be16551129160fa04f6a5
Maharram <98697027+bagisov@users.noreply.github.com>
2022-12-23 09:07:42 +0400

    add Repository interface

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class RegionsController : Controller
+    {
+        private readonly IRegionRepository regionRepository;
+
+        public RegionsController(IRegionRepository regionRepository)
+        {
+            this.regionRepository = regionRepository;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAllRegions()
+        {
+            var regions =  regionRepository.GetAll();
+
+            return Ok(regions);
+        }
+
+
+
+    }
+}
